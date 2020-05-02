e1b2fe2e21fcec62e2d7be57c69864cbcb4e88cc
OOP-IT19780948 <anoli.9926@gmail.com>
2020-05-02 17:18:19 +0530

    oop class dragon

---
+++ me/Random1.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+class Random1  {
+    public static void main(String [] args)
+    {
+        Random RanGen = new Random();
+        int a = RanGen.nextInt(100);
+        System.out.println("Generated" +a);
+
+        for (a = 5);
+        {
+            System.out.println("Generated" +a);
+        } 
+
+    }
+}
